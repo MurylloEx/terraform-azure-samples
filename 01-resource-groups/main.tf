f2b0f4a81bdecaaa0338257b3f5204c32f7aba60
MurylloEx <32225687+MurylloEx@users.noreply.github.com>
2023-10-01 14:54:05 -0300

    feat: created resource-groups sample

---
+++ 01-resource-groups/main.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "~> 3.70"
+    }
+  }
+
+  required_version = "~> 1.4"
+}
+
+provider "azurerm" {
+  subscription_id = var.azure_subscription_id
+  tenant_id = var.azure_tenant_id
+  client_id = var.azure_client_id
+  client_secret = var.azure_client_secret
+  features {}
+}
+
+resource "azurerm_resource_group" "app_group" {
+  name     = "${var.app_stage}-${var.app_name}"
+  location = var.azure_region
+  tags = {
+    Name = var.app_name
+    Stage = var.app_stage
+  }
+}
