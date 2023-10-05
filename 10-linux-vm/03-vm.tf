0bdd9d3681e797560514a9942cdacbba64953cad
MurylloEx <32225687+MurylloEx@users.noreply.github.com>
2023-10-05 19:29:49 -0300

    feat: added sample of cloudinit in linux-vm sample

--- 10-linux-vm/03-vm.tf
+++ 10-linux-vm/03-vm.tf
@@ -8,14 +8,33 @@ resource "local_file" "ssh_private_key" {
   content  = tls_private_key.ssh_key_pair.private_key_pem
 }
 
-resource "azurerm_linux_virtual_machine" "app_linux_vm" {
-  name                  = "${var.app_stage}-${var.app_name}-linux-vm"
-  resource_group_name   = azurerm_resource_group.app_group.name
-  location              = azurerm_resource_group.app_group.location
-  network_interface_ids = [azurerm_network_interface.public_network_interface.id]
-  size                  = "Standard_D2s_v3"
-  admin_username        = "ubuntu"
+data "cloudinit_config" "app_user_data" {
+  gzip          = true
+  base64_encode = true
 
+  part {
+    content_type = "text/cloud-config"
+    content = yamlencode({
+      write_files = []
+      runcmd = [
+        "sudo apt-get update -y",
+        "sudo apt-get upgrade -y",
+        "sudo apt-get install apache2 -y"
+      ]
+    })
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "app_linux_vm" {
+  name                            = "${var.app_stage}-${var.app_name}-linux-vm"
+  resource_group_name             = azurerm_resource_group.app_group.name
+  location                        = azurerm_resource_group.app_group.location
+  network_interface_ids           = [azurerm_network_interface.public_network_interface.id]
+  size                            = "Standard_D2s_v3"
+  admin_username                  = "ubuntu"
+  admin_password                  = "S3cr3t$"
+  user_data                       = data.cloudinit_config.app_user_data.rendered
+  disable_password_authentication = false
 
   admin_ssh_key {
     username   = "ubuntu"
